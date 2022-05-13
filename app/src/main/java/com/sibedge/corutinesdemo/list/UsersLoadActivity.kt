684cca4223cc5c11249ecbff7f4effba90610b1d
Maksim Berezin <bmv_pt2005@mail.ru>
2022-05-13 12:36:04 +0300

    exception handler

--- app/src/main/java/com/sibedge/corutinesdemo/list/UsersLoadActivity.kt
+++ app/src/main/java/com/sibedge/corutinesdemo/list/UsersLoadActivity.kt
@@ -1,9 +1,10 @@
 package com.sibedge.corutinesdemo.list
 
+import android.R
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
-import com.sibedge.corutinesdemo.User
 import com.sibedge.corutinesdemo.databinding.ActivityListBinding
 import com.sibedge.corutinesdemo.log
 import com.sibedge.corutinesdemo.recyclerview.UserAdapter
@@ -27,8 +28,31 @@ class UsersLoadActivity : AppCompatActivity() {
             userAdapter.update(it)
         }
 
+        usersViewModel.getState().observe(this) {
+            log("state = $it")
+            binding.globalJob.setTextColor(colorOnStatus(it.loadJobState))
+            binding.postJob.setTextColor(colorOnStatus(it.posJobState))
+            binding.companyJob.setTextColor(colorOnStatus(it.companyJobState))
+            binding.addJob1.setTextColor(colorOnStatus(it.addJob1))
+            binding.addJob2.setTextColor(colorOnStatus(it.addJob2))
+        }
+
+        usersViewModel.getExcInfo().observe(this) {
+            binding.excText.text = it
+        }
+
         binding.reload.setOnClickListener {
             usersViewModel.reloadData()
+        }
+
+        binding.eraseExcept.setOnClickListener {
+            usersViewModel.erasExc()
+            binding.excText.text = ""
+            binding.globalJob.setTextColor(Color.GRAY)
+            binding.postJob.setTextColor(Color.GRAY)
+            binding.companyJob.setTextColor(Color.GRAY)
+            binding.addJob1.setTextColor(Color.GRAY)
+            binding.addJob2.setTextColor(Color.GRAY)
         }
 
         binding.cancelGeneralJob.setOnClickListener {
@@ -43,8 +67,28 @@ class UsersLoadActivity : AppCompatActivity() {
             usersViewModel.cancelCompanyJob()
         }
 
-        binding.exception.setOnClickListener {
-            usersViewModel.throwException()
+        binding.exceptionGeneral.setOnClickListener {
+            usersViewModel.throwExceptionGeneral()
+        }
+
+        binding.exceptionPost.setOnClickListener {
+            usersViewModel.throwExceptionPost()
+        }
+
+        binding.exceptionCompanyJob.setOnClickListener {
+            usersViewModel.throwExceptionCompanyJob()
+        }
+
+        binding.exceptionAddJob.setOnClickListener {
+            usersViewModel.throwExceptionAddJob()
         }
     }
+
+    private fun colorOnStatus(jobStatus: Status): Int =
+        when (jobStatus) {
+            Status.Undefined -> Color.GREEN
+            Status.Active -> Color.BLUE
+            Status.Cancelled -> Color.RED
+            Status.Completed -> Color.BLACK
+        }
 }
--- app/src/main/java/com/sibedge/corutinesdemo/list/UsersViewModel.kt
+++ app/src/main/java/com/sibedge/corutinesdemo/list/UsersViewModel.kt
@@ -5,54 +5,82 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.sibedge.corutinesdemo.User
-import com.sibedge.corutinesdemo.log
 import com.sibedge.corutinesdemo.repository.DataRepository
 import com.sibedge.corutinesdemo.repository.DataRepositoryImpl
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 import timber.log.Timber
-import java.lang.Exception
 import java.util.*
 
 class UsersViewModel : ViewModel() {
-    companion object {
-        const val TAG = "viewModelScope"
-    }
 
     private val liveData: MutableLiveData<List<User>> = MutableLiveData()
+    private val liveDataJobStates: MutableLiveData<JobsState> = MutableLiveData()
+    private val liveDataExcText: MutableLiveData<String> = MutableLiveData()
     private val dataRepository: DataRepository = DataRepositoryImpl()
-    private lateinit var loadJob: Job
-    private lateinit var postJob: Job
-    private lateinit var companyJob: Job
+    private var loadJob: Job? = null
+    private var postJob: Job? = null
+    private var addJob1: Job? = null
+    private var addJob2: Job? = null
+    private var companyJob: Job? = null
+    private var watchDog: Job? = null
     private var exceptionLevel = 0
     private var shouldThrowException = false
+    private var isWorking = true
 
-    fun getData(): LiveData<List<User>> = liveData
-
-    init {
-        getListOfUses()
+    private val handler = CoroutineExceptionHandler { ctx, exception ->
+        Timber.d("coroutineContext =$ctx exception = $exception")
+        liveDataExcText.postValue(exception.message)
     }
 
-    private fun getListOfUses() {
-        loadJob = viewModelScope.launch(Dispatchers.IO) {
+    fun getData(): LiveData<List<User>> = liveData
+    fun getState(): LiveData<JobsState> = liveDataJobStates
+    fun getExcInfo(): LiveData<String> = liveDataExcText
+
+    init {
+        getListOfUsers()
+        watchDog = viewModelScope.launch(SupervisorJob()) {
+            while (isWorking) {
+                delay(1000)
+                liveDataJobStates.postValue(
+                    JobsState(
+                        Status.onState(loadJob),
+                        Status.onState(postJob),
+                        Status.onState(companyJob),
+                        Status.onState(addJob1),
+                        Status.onState(addJob2)
+                    )
+                )
+            }
+        }
+    }
+
+    private fun getListOfUsers() {
+        loadJob = viewModelScope.launch(Dispatchers.IO + handler) {
             shouldThrowException(0, shouldThrowException)
             val users = dataRepository.getUsers()
 
             companyJob = launch {
-                shouldThrowException(1, shouldThrowException)
                 users.forEach { user ->
                     val userId = user.id
                     delay(1000L)
                     user.myCompany = "Sibedge_$userId"
                     liveData.postValue(users)
                     Timber.d("for user $userId myCompany = ${user.myCompany}")
+                    shouldThrowException(1, shouldThrowException)
+                }
+                addJob1 = launch {
+                    var count = 0
+                    repeat(users.size) {
+                        delay(100L)
+                        count++
+                        shouldThrowException(2, shouldThrowException)
+                    }
+                    Timber.d("addJob1 counted ->$count")
                 }
             }
             postJob = launch {
                 users.forEach { user ->
-                    shouldThrowException(2, shouldThrowException)
+                    shouldThrowException(1, shouldThrowException)
                     delay(1000)
                     val userId = user.id
                     val posts = dataRepository.getPosts(userId)
@@ -60,8 +88,22 @@ class UsersViewModel : ViewModel() {
                     liveData.postValue(users)
                     Timber.d("for user $userId posts.c = ${user.postCounts}")
                 }
+                addJob2 = launch {
+                    var count = 0
+                    repeat(users.size) {
+                        delay(100L)
+                        count++
+                        shouldThrowException(2, shouldThrowException)
+                    }
+                    Timber.d("addJob2 counted ->$count")
+                }
             }
         }
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        isWorking = false
     }
 
     private fun shouldThrowException(currentLevel: Int, should: Boolean) {
@@ -71,23 +113,76 @@ class UsersViewModel : ViewModel() {
 
     fun reloadData() {
         liveData.value = emptyList()
-        getListOfUses()
+        getListOfUsers()
+    }
+
+    fun erasExc() {
+        exceptionLevel = 10
+        loadJob = null
+        postJob = null
+        addJob1 = null
+        addJob2 = null
+        companyJob = null
     }
 
     fun cancelGeneralJob() {
-        loadJob.cancel()
+        loadJob?.cancel()
     }
 
     fun cancelCompanyJob() {
-        companyJob.cancel()
+        companyJob?.cancel()
     }
 
     fun cancelPostJob() {
-        postJob.cancel()
+        postJob?.cancel()
     }
 
-    fun throwException() {
+    fun throwExceptionGeneral() {
+        exceptionLevel = 0
+        shouldThrowException = true
+    }
+
+    fun throwExceptionPost() {
+        exceptionLevel = 1
+        shouldThrowException = true
+    }
+
+    fun throwExceptionCompanyJob() {
+        exceptionLevel = 1
+        shouldThrowException = true
+    }
+
+    fun throwExceptionAddJob() {
         exceptionLevel = 2
         shouldThrowException = true
+    }
+}
+
+data class JobsState(
+    val loadJobState: Status,
+    val posJobState: Status,
+    val companyJobState: Status,
+    val addJob1: Status,
+    val addJob2: Status
+)
+
+enum class Status {
+    Undefined,
+    Active,
+    Cancelled,
+    Completed;
+
+    companion object {
+        fun onState(job: Job?): Status {
+            if (job == null) return Undefined
+            return with(job) {
+                when {
+                    isActive -> Active
+                    isCancelled -> Cancelled
+                    isCompleted -> Completed
+                    else -> Undefined
+                }
+            }
+        }
     }
 }
